86c207ed8d06dadff5a7326e1a30296d0d1be915
Kabukaluca <130925727+Kabukaluca@users.noreply.github.com>
2023-07-22 18:01:33 +0200

    create JS-File with basic math operator functions

---
+++ calculator.js
@@ -0,0 +1,20 @@
+function add(a, b) {
+    return (a + b);
+};
+
+function subtract(a, b) {
+    return (a - b);
+};
+
+function muliply(a, b) {
+    return (a * b);
+};
+
+function divide(a, b) {
+    return (a / b);
+};
+
+console.log(add(1,2));
+console.log(subtract(2,1));
+console.log(muliply(2,2));
+console.log(divide(6,2));
