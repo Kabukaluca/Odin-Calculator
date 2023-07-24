338d38435d678f6bd6afe7ba67c65bf168327a31
Kabukaluca <130925727+Kabukaluca@users.noreply.github.com>
2023-07-24 20:54:39 +0200

    Add display functions
    create function that update display according to the clicked number for better UE and visibility;
    
    after clicking an operator button currentDisplay value will be transfered to previousDisplay;
    That way it is possible to see the first entered number while entering the next;
    
    add delete btn which cleares the last number of currentDisplay;
    add ac btn which cleares current and previous display to reset the calculator;
    
    [Ticket: X]

--- calculator.js
+++ calculator.js
@@ -1,10 +1,44 @@
+// Event listeners
+const numBtns = document.querySelectorAll(".number-btn");
+const currentDisplay = document.querySelector(".display-current");
+const previousDisplay = document.querySelector(".display-previous");
+const delBtn = document.querySelector(".del-btn");
+const acBtn = document.querySelector(".ac-btn");
+const operatorBtns = document.querySelectorAll(".operator-btn");
+
+// DOM Methods
+numBtns.forEach(numBtn => {
+numBtn.addEventListener("click", () => {
+    const buttonValue = numBtn.textContent;
+    currentDisplay.textContent += buttonValue;
+    })
+});
+
+delBtn.addEventListener("click", () => {
+    currentDisplay.textContent = currentDisplay.textContent.slice(0, -1)
+});
+
+acBtn.addEventListener("click", () => {
+    currentDisplay.textContent = "";
+    previousDisplay.textContent = "";
+});
+operatorBtns.forEach(operatorBtn => {
+operatorBtn.addEventListener("click", () => {
+    const opBtnVal = operatorBtn.textContent;
+    currentDisplay.textContent += " " + opBtnVal;
+    previousDisplay.textContent = currentDisplay.textContent;
+    currentDisplay.textContent = "";
+    })
+});
+
+
 // Variables
 var firstNum = "";
 var secondNum = "";
 var operator = "";
 
 function operate(firstNum, operator, secondNum) {
-    if (operator === "+") {return add(firstNum, secondNum)};
+    if (operator === "+")  {return add(firstNum, secondNum)};
     if (operator === "-") {return subtract(firstNum, secondNum)};
     if (operator === "/") {return divide(firstNum, secondNum)};
     if (operator === "*") {return  multiply(firstNum, secondNum)};
